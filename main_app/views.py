90e075d806c2794c874b1688e6d886c289549eea
HemanthSherla <sherlahemanth001@gmail.com>
2021-06-27 13:45:34 +0530

    commit

---
+++ main_app/views.py
@@ -0,0 +1,191 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import JsonResponse
+from datetime import date
+
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import patient , doctor 
+
+#Hemanth
+# Create your views here.
+
+def home(request):
+
+  if request.method == 'GET':
+        
+      if request.user.is_authenticated:
+        return render(request,'homepage/index.html')
+
+      else :
+        return render(request,'homepage/index.html')
+# -------------------------------------- ADMIN ------------------------------------------------
+
+def admin_ui(request):
+
+    if request.method == 'GET':
+
+      if request.user.is_authenticated:
+
+        auser = request.user
+      else :
+        return redirect('home')
+
+
+
+    if request.method == 'POST':
+
+       return render(request,'patient/patient_ui/profile.html')
+
+
+#--------------------------------- PATIENT -----------------------------------------------------
+
+
+
+
+
+def patient_ui(request):
+
+    if request.method == 'GET':
+
+      if request.user.is_authenticated:
+
+        patientusername = request.session['patientusername']
+        puser = User.objects.get(username=patientusername)
+
+        return render(request,'patient/patient_ui/profile.html' , {"puser":puser})
+
+      else :
+        return redirect('home')
+
+
+
+    if request.method == 'POST':
+
+       return render(request,'patient/patient_ui/profile.html')
+
+       
+
+
+def pviewprofile(request, patientusername):
+
+    if request.method == 'GET':
+
+          puser = User.objects.get(username=patientusername)
+
+          return render(request,'patient/view_profile/view_profile.html', {"puser":puser})
+
+
+
+
+
+   
+    
+
+
+#--------------------------------- DOCTOR ---------------------------------------------------
+
+def doctor_ui(request):
+
+    if request.method == 'GET':
+
+      doctorid = request.session['doctorusername']
+      duser = User.objects.get(username=doctorid)
+
+    
+      return render(request,'doctor/doctor_ui/profile.html',{"duser":duser})
+
+
+
+      
+
+
+def dviewprofile(request, doctorusername):
+
+    if request.method == 'GET':
+
+         
+         duser = User.objects.get(username=doctorusername)
+        
+       
+         return render(request,'doctor/view_profile/view_profile.html', {"duser":duser} )
+
+
+#-----------------------------------------Checkdisease------------------------------------------------
+def checkdisease(request):
+
+  diseaselist=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction','Peptic ulcer diseae','AIDS','Diabetes ',
+  'Gastroenteritis','Bronchial Asthma','Hypertension ','Migraine','Cervical spondylosis','Paralysis (brain hemorrhage)',
+  'Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A', 'Hepatitis B', 'Hepatitis C', 'Hepatitis D',
+  'Hepatitis E', 'Alcoholic hepatitis','Tuberculosis', 'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
+  'Heart attack', 'Varicose veins','Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
+  'Arthritis', '(vertigo) Paroymsal  Positional Vertigo','Acne', 'Urinary tract infection', 'Psoriasis', 'Impetigo']
+
+
+  symptomslist=['itching','skin_rash','nodal_skin_eruptions','continuous_sneezing','shivering','chills','joint_pain',
+  'stomach_pain','acidity','ulcers_on_tongue','muscle_wasting','vomiting','burning_micturition','spotting_ urination',
+  'fatigue','weight_gain','anxiety','cold_hands_and_feets','mood_swings','weight_loss','restlessness','lethargy',
+  'patches_in_throat','irregular_sugar_level','cough','high_fever','sunken_eyes','breathlessness','sweating',
+  'dehydration','indigestion','headache','yellowish_skin','dark_urine','nausea','loss_of_appetite','pain_behind_the_eyes',
+  'back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+  'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+  'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+  'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+  'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+  'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+  'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+  'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+  'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+  'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+  'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+  'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+  'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+  'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+  'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+  'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+  'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+  'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+  'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+  'yellow_crust_ooze']
+
+  alphabaticsymptomslist = sorted(symptomslist)
+
+  
+
+
+  if request.method == 'GET':
+    
+     return render(request,'patient/checkdisease/checkdisease.html', {"list2":alphabaticsymptomslist})
+
+
+
+
+  elif request.method == 'POST':
+       
+      ## access you data by playing around with the request.POST object
+      
+      inputno = int(request.POST["noofsym"])
+      print(inputno)
+      if (inputno == 0 ) :
+          return JsonResponse({'predicteddisease': "none",'confidencescore': 0 })
+  
+      else :
+
+        psymptoms = []
+        psymptoms = request.POST.getlist("symptoms[]")
+       
+        print(psymptoms)
+
+
+
+
+
+
+
+
+    
+
+
+#-----------------------------chatting system ---------------------------------------------------
+
+
